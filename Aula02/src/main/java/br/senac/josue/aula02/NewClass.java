c834132f001a72a5d531c122dddc030520b6736e
josue.minacio <josue.minacio@CAS511744W0725.senacsp.edu.br>
2017-08-15 21:50:16 -0300

    Projeto java inicial

--- Aula02/src/main/java/br/senac/josue/aula02/NewClass.java
+++ Aula02/src/main/java/br/senac/josue/aula02/NewClass.java
@@ -12,5 +12,6 @@ package br.senac.josue.aula02;
 public class NewClass {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 }
